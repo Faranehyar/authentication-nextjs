cee7ab7d5563abf56c7435d3a921dc5839139c1c
faraneh <faranehyarahmadi@gmail.com>
2025-07-16 14:27:04 +0330

    full update commit

---
+++ app/auth/login/page.js
@@ -0,0 +1,45 @@
+"use client"
+
+import { login } from "@/actions/auth";
+import SubmitButton from "@/components/SubmitButton";
+import AuthContext from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+import { useFormState } from "react-dom"
+import { toast } from "react-toastify";
+
+export default function Login() {
+    const [state, formAction] = useFormState(login, {});
+    const router = useRouter();
+    const { loginContext } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (state?.error) {
+            toast.error(state?.error)
+        } else if (state?.success) {
+            toast.success(state?.success)
+            loginContext(state?.user)
+            router.push("/")
+        }
+    }, [state])
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-4">
+                    <form action={formAction}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email address</label>
+                            <input name="email" type="email" className="form-control" id="email" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input name="password" type="password" className="form-control" id="password" />
+                        </div>
+                        <SubmitButton title="login" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
